0d95dbe8440f7242b022061c3d69e008e0f276d3
bagas-sanjaya-UeU <charderrasanjaya@student.esaunggul.ac.id>
2024-03-16 16:33:28 +0700

    pert 1

---
+++ pert 1/main.cpp
@@ -0,0 +1,66 @@
+// startup code c++ for pert 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // tipe data integer (bilangan bulat)
+    int angka_1 = 10;
+    int angka_2 = 20;
+
+    short int si = 3000;
+    long int li = 3000000000;
+
+    // tipe data float (bilangan pecahan)
+    float pecahan_1 = 10.5;
+    float pecahan_2 = 20.5;
+    long double ld = 30.5;
+
+    // tipe data char (karakter)
+    char karakter_A = 'A';
+    char karakter_B = 'B';
+
+    // tipe data boolean (benar atau salah)
+    bool benar = true;
+    bool salah = false;
+
+    // tipe data string (kumpulan karakter)
+    string sapa = "Hello World";
+
+    // tipe data array (kumpulan tipe data)
+    int array[5] = {1, 2, 3, 4, 5};
+
+    // tipe data pointer (alamat memori)
+    int *pointer;
+    int *pointer_2;
+
+    // // tipe data struct (struktur data)
+    // struct buah
+    // {
+    //     string nama;
+    //     int harga;
+    // };
+
+    // // tipe data class (kelas)
+    // class mobil
+    // {
+    //     string merk;
+    //     int harga;
+    // };
+
+    // tipe data function (fungsi)
+    // void sapa(int a, int b)
+    // {
+    //     cout << "Hello World" << a << b << endl;
+    // }
+    // sapa(11, 12);
+
+    // print di console
+    // cout << angka_1 << endl;
+    // cout << "Size bit dari short int: " << sizeof(si) << endl;
+    // cout << "Size bit dari long int: " << sizeof(li) << endl;
+
+    return 0;
+}
